980ac60ed96ff661236664d5a59e2804d633dcd3
Maicon Lourenço <maiconloure@gmail.com>
2022-03-02 19:57:01 -0300

    added Button and SignInBox

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { View, Text } from "react-native"
 import { Header } from "../../components/Header"
 import { MessageList } from "../../components/MessageLIst"
+import { SignInBox } from "../../components/SignInBox"
 import { styles } from "./styles"
 
 export function Home() {
@@ -9,6 +10,8 @@ export function Home() {
     <View style={styles.container}>
       <Header />
       <MessageList />
+
+      <SignInBox />
     </View>
   )
 }
